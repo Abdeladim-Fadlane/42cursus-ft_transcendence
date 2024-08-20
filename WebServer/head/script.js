41e8ee5fb02f60d8a99141d3deb8ee817120a39e
yakhay <khayyassine450@gmail.com>
2024-08-20 18:00:21 +0100

    last

--- WebServer/head/script.js
+++ WebServer/head/script.js
@@ -1,3 +1,72 @@
+function loadHomeContent() {
+    document.getElementById('head-page').style.display = 'none';
+    document.getElementById('home-page').style.display = 'block';
+    fetch("../home/index.html")
+        .then(response => response.text())
+        .then(html => {
+            // Insert the loaded content into the home container
+            document.getElementById("home-page").innerHTML = html;
+
+            // Hide login container and show home container
+            
+
+            // Enable the home CSS
+            // document.getElementById("home-style").disabled = false;
+        })
+        .then(() => {
+            loadCSS('../home/style.css');
+                removeCSS('./index.css');
+                
+        })
+        .then(() => {
+            const script = loadScript('./script2.js');
+                document.head.appendChild(script);
+                const invite = loadScriptnotmodul('./invite.js');
+                document.head.appendChild(invite);
+                const leadr = loadScriptnotmodul('./leader.js');
+                document.head.appendChild(leadr);
+                const data = loadScriptnotmodul('./data.js');
+                document.head.appendChild(data);
+                const match = loadScriptnotmodul('./match.js');
+                document.head.appendChild(match);
+                const notifi = loadScriptnotmodul('./notif.js');
+                document.head.appendChild(notifi);
+                const suggest = loadScriptnotmodul('./suggest.js');
+                document.head.appendChild(suggest);
+                const msgfriend = loadScriptnotmodul('./msgfriend.js');
+                document.head.appendChild(msgfriend);
+                const track = loadScriptnotmodul('./track.js');
+                document.head.appendChild(track);
+                const chatScript = loadScriptnotmodul('./chatScript.js');
+                document.head.appendChild(chatScript);
+                const setting = loadScript('./setting.js');
+                document.head.appendChild(setting);
+                const game = loadScript('./game.js');
+                document.head.appendChild(game);
+                const search = loadScriptnotmodul('./search.js');
+                document.head.appendChild(search);
+                const userInformation = loadScriptnotmodul('./userInformation.js');
+                document.head.appendChild(userInformation);
+                const ProfileAction = loadScriptnotmodul('./ProfileAction.js');
+                document.head.appendChild(ProfileAction);
+                
+        })
+        .catch(error => console.error('Error loading home content:', error));
+}
+function loadCSS(file) {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = file;
+    document.head.appendChild(link);
+}
+function removeCSS(file) {
+    const linkElement = document.querySelector(`link[href="${file}"]`);
+    if (linkElement) {
+        linkElement.parentNode.removeChild(linkElement);
+    }
+}
+
+
 function already_logged() {
     fetch('/api/already_logged/')
         .then(response => {
@@ -6,12 +75,34 @@ function already_logged() {
             }
         })
         .then(data => {
-            if (data.status === true)
-                window.location.href = "/home/";
+            // console.log(data);
+            
+            if (data.status === true){
+                 loadHomeContent();
+        }
+            
         })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+function loadScript (src){
+    const script = document.createElement('script');
+    script.src = src;
+    script.defer = true;
+    // script.type = 'module';
+    return script;
+};
+
+function loadScriptnotmodul (src){
+    const script = document.createElement('script');
+    script.src = src;
+    script.defer = true;
+    script.type = 'module';
+    return script;
+};
+
+
+document.addEventListener('DOMContentLoaded', () => {
+   
     already_logged();
     fetch('/api/csrf-token/')
         .then(response => response.json())
@@ -20,26 +111,33 @@ document.addEventListener('DOMContentLoaded', function() {
         })
         .catch(error => console.error('Error fetching CSRF token:', error));
     });
-    document.getElementById('login-form-id').addEventListener('submit', function(event) {
+    document.getElementById('login-form-id').addEventListener('submit', async function(event) {
         event.preventDefault();
         const formData = new FormData(this);
         const csrfToken = document.getElementById('csrfToken1').value;
-        fetch('/loginuser/', {
-            method: 'POST',
-            body: formData,
-            headers: {'X-CSRFToken': csrfToken,}
-        })
-        .then(response => response.json())
-        .then(data => {
+    
+        try {
+            const response = await fetch('/loginuser/', {
+                method: 'POST',
+                body: formData,
+                headers: {'X-CSRFToken': csrfToken}
+            });
+            
+            const data = await response.json();
+    
             if (data.status === true) {
-                    window.location.href = "/home/";
+                loadHomeContent(); // Assuming loadHomeContent is an async function
             } else {
                 document.getElementById('messages').innerHTML = 'Invalid username or password';
                 document.getElementById('messages').style.color = 'red';
             }
-        })
-
-});
+        } catch (error) {
+            console.error('Error during login:', error);
+            document.getElementById('messages').innerHTML = 'An error occurred';
+            document.getElementById('messages').style.color = 'red';
+        }
+    });
+    
 
 function ft_sign_up() {
     
@@ -47,6 +145,7 @@ function ft_sign_up() {
     modal1.style.display = 'none';
     const modal = document.getElementById('sign-up-form');
     modal.style.display = 'flex';
+    window.history.pushState({page: ''}, '', '?page=sign-up');
     
   }
 
@@ -58,6 +157,7 @@ const modal1 = document.getElementById('sign-up-form');
 modal1.style.display = 'none';
 const modal = document.getElementById('login-form');
 modal.style.display = 'flex';
+window.history.pushState({page: ''}, '', '?page=login');
 }
   function closeLogoutModal() {
     
---
+++ WebServer/head/track.js
@@ -0,0 +1,174 @@
+import { handlenotif } from './notif.js';
+import { fetchdelette } from './suggest.js';
+import { fetchSuggestions } from './invite.js';
+import { fetchHistory } from './match.js';
+import { handlechalleng } from './challenge.js';
+import { leaderboard_requests } from './leader.js';
+import { fetchAndUpdateFriends , fetchOnlineFriendInChat} from './msgfriend.js';
+import { fetchConversation, fetchAllMessage} from './chatScript.js';
+import { to_chat } from './ProfileAction.js';
+
+import { ProfileUsername , ProfileUser_id, button_profile, ProfileStutus} from './userInformation.js';
+let user_id = document.querySelector('#login');
+let user_name = document.querySelector('#login');
+let contentUserElement = document.querySelector('#content-user');
+
+if (contentUserElement) {
+    let Profile_module = window.getComputedStyle(contentUserElement);
+    // Now you can work with Profile_module
+}
+
+let area = document.querySelector('.carte-message');
+
+// window.onload = fetchSuggestions;
+// window.onload = handlenotif;
+// window.onload = fetchdelette;
+// window.onload = fetchHistory;
+// window.onload = handlechalleng;
+// window.onload = leaderboard_requests;
+
+
+
+
+
+
+function show_message(content, username){
+    
+    const now = new Date();
+    // console.log('=======> ' + username);
+    fetch('/api/csrf-token/')
+    .then(response =>{
+        return response.json();
+    })
+    .then(data =>{
+        fetch('/api/friend/', {
+            method: 'POST',
+            headers : {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': data.csrfToken,
+            },
+            body :JSON.stringify({
+                'username' : username,
+            })
+        })
+        .then(response =>{
+            return response.json();
+        })
+        .then(data=>{
+            // console.log(data);
+            area.id = data.id;
+            area.querySelector('.carte-user-profile').querySelector('img').src = data.photo_profile;
+        })
+    })
+    // console.log(area);
+    area.querySelector('.carte-sender-name').textContent = username;
+    if (content.length >= 145)
+        area.querySelector('.carte-message-content').textContent = content.substring(0, 145) + ' ...';
+    else
+        area.querySelector('.carte-message-content').textContent = content;
+    document.querySelector('.carte-date').textContent = `${now.toLocaleTimeString()}`;
+    area.addEventListener('click', (e) =>{
+        area.style.display = 'none';
+        let chat_aside = document.querySelector('.chat-aside');
+        chat_aside.click();
+        let friends = document.querySelectorAll('.friend-list-room');
+        for (let j = 0; j < friends.length; j++)
+            if (friends[j].id == area.id)
+                friends[j].click();
+    });
+    area.style.display = 'flex';
+    setTimeout(()=>{area.style.display = 'none';}, 5000)
+}
+fetch('/api/token/')
+    .then(response => response.json())
+    .then(data => {
+       let token = data.token;
+        let id = data.id;
+        const socket = new WebSocket(`wss://${window.location.host}/wss/track/?token=${token}&id=${id}`);
+        socket.onopen = () => {
+            console.log('WebSocket connected');
+        };
+        socket.onclose = () => {
+            console.log('WebSocket closed');
+        };
+        
+        socket.onerror = (error) => {
+            console.error('WebSocket error: ', error);
+        };
+        socket.onmessage =  (event) => {
+            
+            const data = JSON.parse(event.data);
+            console.log(data.message);
+            if (typeof(data) == 'object' && data.message.message === "friend send message"){
+                console.log('====> socket track message')
+                fetchConversation(user_id.className, user_name.textContent)
+                fetchAllMessage(user_id.className, user_name.textContent);
+                // console.log(data.message.message_content);
+                // console.log(data.message.sender_name);
+                if (window.getComputedStyle(document.getElementById("chat")).display == 'none')
+                    show_message(data.message.message_content, data.message.sender_name)
+            }
+            else if (data.message === 'friend_request_send') {
+                handlenotif();
+                if (Profile_module.display == 'flex')
+                    button_profile(ProfileUsername, ProfileUser_id);
+            }
+            else if (data.message === 'friend_request_reject') {
+                fetchSuggestions();
+                if (Profile_module.display == 'flex')
+                    button_profile(ProfileUsername, ProfileUser_id);
+
+            }
+            else if (data.message === 'friend_request_accept') {
+                fetchdelette();
+                fetchAndUpdateFriends();
+                handlechalleng();
+                if (Profile_module.display == 'flex')
+                    button_profile(ProfileUsername, ProfileUser_id);
+            }
+            else if (data.message === 'friend_request_suggest') {
+
+                fetchSuggestions();
+                leaderboard_requests();
+                if (Profile_module.display == 'flex')
+                    button_profile(ProfileUsername, ProfileUser_id);
+            }
+            else if (data.message === 'friend is online' || data.message === 'friend is offline') {
+                console.log('online friends====>' );
+                fetchOnlineFriendInChat();
+                handlechalleng();
+                if (Profile_module.display == 'flex')
+                    ProfileStutus(ProfileUsername);
+            }
+            else if (data.message === 'profile_change') {
+                handlenotif();
+                fetchSuggestions();
+                leaderboard_requests();
+                handlechalleng();
+                fetchHistory();
+                fetchdelette();
+                fetchAndUpdateFriends();
+                // data();
+                
+            }
+            else if (data.message === 'update_leaderboard') {
+                leaderboard_requests();
+
+            }
+            else if (data.message === 'update_match_history') {
+                fetchHistory();
+            }
+            else if (data.message === 'friend_delete') {
+                fetchConversation(user_id.className, user_name.textContent)
+                fetchdelette();
+                fetchSuggestions();
+                fetchAndUpdateFriends();
+                handlechalleng();
+                if (Profile_module.display == 'flex')
+                    button_profile(ProfileUsername, ProfileUser_id);
+            }
+        }
+});
+
+
+       
